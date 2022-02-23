966e3e7ba410b268637b10eb055bfb36c3f3452f
ksXV <msgcipi@gmail.com>
2022-02-23 19:53:00 +0200

    added the cart dropdown feature 'semi'

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,12 +1,21 @@
 import CustomButton from "../custom-button/custom-button.component";
-import "./cart-dropdown.styles.scss";
 import React from "react";
-
-export const CartDropdown = () => {
+import { connect } from "react-redux";
+import CartItem from "../cart-item/cart-item.component";
+import "./cart-dropdown.styles.scss";
+const CartDropdown = ({ cartItems }) => {
   return (
     <div className="cart-dropdown">
-      <div className="cart-item"></div>
+      <div className="cart-items">
+        {cartItems.map((cartItem) => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))}
+      </div>
       <CustomButton>Go to checkout</CustomButton>
     </div>
   );
 };
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems,
+});
+export default connect(mapStateToProps)(CartDropdown);
